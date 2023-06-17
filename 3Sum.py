232fa1b86116d7ae1f3a0e3a3e2c2adc8b1475ab
Mack Shonayi <shonayimack@gmail.com>
2023-06-18 01:06:29 +0200

    Aded function to fin 3 sums that add to 0 and another that accepts target

--- 3Sum.py
+++ 3Sum.py
@@ -29,3 +29,60 @@ Constraints:
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105
 """
+def threeSum(nums):
+    res = []
+    nums.sort()
+
+    for i, a in enumerate(nums):
+        if i > 0 and nums[i] == nums[i - 1]: # skip duplicates
+            continue
+
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = a + nums[l] + nums[r]
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
+            else:
+                res.append([a, nums[l], nums[r]])
+                # Updating pointers
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return res
+
+def threeSumWTarget(nums, target):
+    res = []
+    nums.sort()
+
+    for i, a in enumerate(nums):
+        if i > 0 and nums[i] == nums[i - 1]: # skip if the value is already used
+            continue
+
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = a + nums[l] + nums[r]
+            if threeSum > target:
+                r -= 1
+            elif threeSum < target:
+                l += 1
+            else:
+                res.append([a, nums[l], nums[r]])
+                # Updating pointers
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return res
+
+
+nums = [-1, 0, 1, 2, -1, -4]
+target = 0
+## [[-1,-1,2],[-1,0,1]]
+print(threeSum(nums, target))
+
+
+
+nums = [-1,0,1,2,-1,-4]
+## [[-1,-1,2],[-1,0,1]]
+print(threeSum(nums))

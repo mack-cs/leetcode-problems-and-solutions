e9eabb5186734e549eeffd32ea5e51257ab240e1
Mack Shonayi <shonayimack@gmail.com>
2023-07-02 23:47:58 +0200

    New Solutions

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -38,7 +38,15 @@ def search(nums, target):
         #left sorted portion
         if nums[l] <= nums[mid]:
             if target > nums[mid] or target < nums[l]:
-                left = mid + 1
-            elif target < nums[1]:
-                
+                l = mid + 1
+            else:
+                r = mid - 1
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+    return -1
+
+
         

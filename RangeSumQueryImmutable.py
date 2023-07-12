2d2b3cfee6621778384c58530e680a76e7657b36
Mack Shonayi <shonayimack@gmail.com>
2023-07-12 23:36:22 +0200

    Add new sulutions

---
+++ RangeSumQueryImmutable.py
@@ -0,0 +1,53 @@
+"""
+Range Sum Query - Immutable
+Easy
+2.9K
+1.8K
+Companies
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ 
+
+Constraints:
+"""
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = []
+        cur = 0
+        for n in nums:
+            cur += n
+            self.prefix.append(cur)
+
+    def sumRange(self, left: int, right: int) -> int:
+        rightSum = self.prefix[right]
+        leftSum = self.prefix[left - 1] if left > 0 else 0
+        return rightSum - leftSum
+    
+
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
